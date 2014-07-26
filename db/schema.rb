c46fbaf161ddf19c617a53e251a0905a156b5fde
ianrtracey <ianrtracey@gmail.com>
2014-07-26 00:29:20 -0700

    CSS style changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140726061112) do
+ActiveRecord::Schema.define(version: 20140726065608) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20140726061112) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.date     "date"
+  end
+
+  create_table "static_pages", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
